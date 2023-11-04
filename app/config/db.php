e3500304e02ff357e8f5d3fa427e0d849c9d0218
B-MaTe <balazs.mate2002@gmail.com>
2023-11-04 18:46:20 +0100

    first test commit with some basic files

---
+++ app/config/db.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    private $connection;
+
+    public function __construct($host, $username, $password, $database)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+
+        $this->connect();
+    }
+
+    public function __destruct() {
+        if ($this->connection->ping()) {
+            $this->close();
+        }
+    }
+
+    private function connect()
+    {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public function query($sql)
+    {
+        return $this->connection->query($sql);
+    }
+
+    public function close()
+    {
+        $this->connection->close();
+    }
+}
