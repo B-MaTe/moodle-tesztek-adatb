0e555058c53983b5e8c405b31e8e2d1737589542
B-MaTe <balazs.mate2002@gmail.com>
2023-11-04 18:47:39 +0100

    first test commit with some basic files

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Moodle Teszt</title>
+</head>
+<body>
+<header>
+    <h1>Moodle Teszt</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
+
+<main>
+    <section>
+        <h2>About Us</h2>
+        <p>This is the about us section of your website.</p>
+    </section>
+
+    <section>
+        <h2>Contact Us</h2>
+        <p>Feel free to get in touch with us!</p>
+    </section>
+</main>
+
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Your Website Name</p>
+</footer>
+</body>
+</html>
